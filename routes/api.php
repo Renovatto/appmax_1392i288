2738d59d29f64d1d2db5efc439d83e08d656d291
Andre Renovato <andre.renovatto@uol.com.br>
2022-01-25 07:00:20 -0300

    configuração e definição de rotas api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('addProduct','ApiController@addProduct');
+Route::post('moveStock','ApiController@moveStock');
+Route::get('history','ApiController@historyStock');
