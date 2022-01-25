37a0b0f399fdbf21393ee6839796321d40178013
Andre Renovato <andre.renovatto@uol.com.br>
2022-01-25 07:01:40 -0300

    criação de migration para tabela produtos

--- database/migrations/2022_01_23_123756_create_products_table.php
+++ database/migrations/2022_01_23_123756_create_products_table.php
@@ -16,8 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('sky');
-            $table->integer('qtd_ini');
+            $table->string('sku');
             $table->timestamps();
         });
     }
